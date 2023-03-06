ad88570630645e7ddf969c25d0e84b60f45959d5
Leonardo Santos <lsantos@esapiens.com.br>
2023-03-06 15:21:22 -0400

    criando arquivos de rotas

--- public/index.php
+++ public/index.php
@@ -1,15 +1,5 @@
 <?php
 
-use Slim\Factory\AppFactory;
-use App\Controllers\Product\ProductController;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = AppFactory::create();
-
-// SET ROUTES
-$app->get('/products', [ProductController::class, 'index'])->setName("products.index");
-$app->post('/products', [ProductController::class, 'store'])->setName("products.store");
-
-$app->run();
+// includes routes file
+require __DIR__ . '/../routes/api.php';
 
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use App\Controllers\Product\ProductController;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// SET ROUTES
+$app->get('/products', [ProductController::class, 'index'])->setName("products.index");
+$app->post('/products', [ProductController::class, 'store'])->setName("products.store");
+
+$app->run();
